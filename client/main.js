3d81a3e86cbbde6b53d586510fd0fea7a5889d5b
Chris Knoll <cknoll3@gmail.com>
2016-04-30 04:34:08 -0700

    Added chat capability.  Added character information to session data and called appropriately when displaying.

--- client/main.js
+++ client/main.js
@@ -3,3 +3,57 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+var character = {name: "Jane", stamina: 23, money: 10300};
+Session.set('character', character);
+
+Template.addMessageForm.onCreated(function() {
+  //save some initial data for our messaging application
+  Session.setDefault('messages', []);
+});
+
+Template.addMessageForm.events({
+  'submit .newMessage': function(event, template) {
+    //prevent the form from refreshing the page
+    event.preventDefault();
+
+    //get our form value (message text)
+    var messageText = $('#messageText').val();
+    $('#messageText').val(''); // remove text from our message box
+
+    //get our data source (from session)
+    var messages = Session.get('messages');
+
+    //save our message
+    messages.push({
+      messageText: messageText,
+      name: character.name
+    });
+
+    Session.set('messages', messages);
+  }
+});
+
+Template.messageList.helpers({
+  allMessages: function() {
+    return Session.get('messages');
+  }
+});
+
+Template.player.helpers({
+  stamina: function() {
+    return Session.get('character').stamina;
+  },
+
+  money: function() {
+    return Session.get('character').money;
+  },
+
+  name: function() {
+    return Session.get('character').name;
+  }
+})
+
+//global helpers
+Template.registerHelper('messagesExist', function() {
+  return Session.get('messages').length > 0;
+});
